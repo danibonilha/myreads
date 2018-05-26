ddc8b01ebd1aa36abbe1eb0de149bac94fe7f2cc
danibonilha <lopesbond7@students.rowan.edu>
2018-05-25 23:19:28 -0300

    feat: Display search results

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import '../scenes/App.css';
+import { PropTypes } from 'prop-types';
 
 const SearchBar = (props) => (
 	<div className="search-books-bar">
 		<a className="close-search" >Close</a>
 		<div className="search-books-input-wrapper">
-			<input type="text" placeholder={props.placeholder} />
+			<input 
+				type="text" 
+				placeholder={props.placeholder} 
+				onChange={(event) => props.updateQuery(event.target.value)}/>
 		</div>
 	</div>
 );
 
-export  {SearchBar};
+SearchBar.propTypes = {
+	placeholder: PropTypes.string,
+	updateQuery: PropTypes.func.isRequired
+};
+
+export  { SearchBar };
--- src/scenes/SearchBooks.js
+++ src/scenes/SearchBooks.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import * as BooksAPI from '../services/BooksAPI';
 import './App.css';
-import { SearchBar } from './../components';
+import { SearchBar, ListBooks } from './../components';
+
 
 class SearchBooks extends React.Component {
+	constructor(props){
+		super(props);
+		this.updateQuery = this.updateQuery.bind(this);
+	}
+	
+	state = {
+		query: '',
+		books: []
+	}
+
+	updateQuery = (query) => {
+		this.setState({query: query.trim()});
+		BooksAPI.search(query).then((books) => this.setState({ books }));
+	}
 	render() {
 		return (
 			<div className="search-books">
-				<SearchBar placeholder="Search by title or author" />
+				<SearchBar 
+					placeholder="Search by title or author"
+					updateQuery={this.updateQuery} />
 				<div className="search-books-results">
-					<ol className="books-grid"></ol>
+					<ListBooks books={this.state.books}/>
 				</div>
 			</div>
 		);
