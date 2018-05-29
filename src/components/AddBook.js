e7b9202751fe77ebbaabf5a21cac0f4a0e1c9e20
danibonilha <lopesbond7@students.rowan.edu>
2018-05-28 23:28:05 -0300

    refactor: Remove useless code

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -2,10 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/App.css';
 
-const AddBook = (props) => (
+const AddBook = () => (
 	<Link
 		to='/search'
-		onClick={props.onNavigate}
 		className='open-search'
 	>Add a book</Link>
 );
