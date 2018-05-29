c37652dd8969537103fb85335e5874a4cf58a15c
danibonilha <lopesbond7@students.rowan.edu>
2018-05-29 00:00:45 -0300

    feat: Convert SelecShelf to controlled component to keep track of current shelf value

--- src/components/SelectShelf.js
+++ src/components/SelectShelf.js
@@ -1,29 +1,45 @@
-import React from 'react';
+import React  from 'react';
 import '../styles/App.css';
 import { PropTypes } from 'prop-types';
 
 
-const SelectShelf = ({ shelf, changeShelf}) => (
-	<select 
-		value={shelf} 
-		onChange={(event) => changeShelf(event.target.value)}
-	>
-		<option disabled>Move to...</option>
-		<option value="currentlyReading">Currently Reading</option>
-		<option value="wantToRead">Want to Read</option>
-		<option value="read">Read</option>
-		<option value="none">None</option>
-	</select>
-);
+class SelectShelf extends React.Component {
+	static propTypes = {
+		shelf: PropTypes.string,
+		changeShelf: PropTypes.func.isRequired		
+	};
+	
+	static defaultProps = {
+		shelf: 'none'
+	};
 
-SelectShelf.propTypes = {
-	shelf: PropTypes.string,
-	changeShelf: PropTypes.func.isRequired		
-};
+	constructor(props) {
+		super(props);    
+		this.handleChange = this.handleChange.bind(this);
+	}
+	state = {
+		currentShelf: this.props.shelf
+	}
+	handleChange(event) {
+		const shelf = event.target.value;
+		this.setState({currentShelf: shelf});
+		this.props.changeShelf(shelf);
+	}
 
-SelectShelf.defaultProps = {
-	shelf: 'none'
-};
-
+	render() {
+		return (
+			<select 
+				value={this.state.currentShelf} 
+				onChange={this.handleChange}
+			>
+				<option disabled>Move to...</option>
+				<option value="currentlyReading">Currently Reading</option>
+				<option value="wantToRead">Want to Read</option>
+				<option value="read">Read</option>
+				<option value="none">None</option>
+			</select>
+		);
+	}
+}
 
 export { SelectShelf };
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from '../scenes/App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../App';
 
 /** 
  This course is not designed to teach Test Driven Development. 
@@ -9,8 +9,8 @@ import App from '../scenes/App'
 **/
 
 it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-})
+	const div = document.createElement('div');
+	ReactDOM.render(<App />, div);
+});
 
 
