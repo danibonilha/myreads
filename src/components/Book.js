fa53fbdbaa5af6e088686a25dcf6912466fc5078
danibonilha <lopesbond7@students.rowan.edu>
2018-05-31 21:19:23 -0300

    feat: Change selectShelf and Book style

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,45 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
 import { SelectShelf } from './';
 import '../styles/App.css';
+import { Dimmer, Image } from 'semantic-ui-react';
 
 
-
-const Book = ({ book, onUpdateShelf }) => {
-	const coverStyle = {
-		width: 128,
-		height: 193,
-		backgroundImage: `url(${book.imageLinks ?
-			book.imageLinks.thumbnail : require('../icons/no-cover.svg')})`
+class Book extends Component {
+	state = {}
+	
+	changeShelf = (shelf) => {
+		this.props.onUpdateShelf(this.props.book, shelf);
 	};
+	handleShow = () => this.setState({ active: true })
+	handleHide = () => this.setState({ active: false })
 
-	const changeShelf = (shelf) => {
-		onUpdateShelf(book, shelf);
-	};
+	render() {
+		const { book } = this.props;
+		const { active } = this.state;
+		const shelfSelector = (
+			<SelectShelf
+				shelf={book.shelf}
+				changeShelf={this.changeShelf}
+			/>);
+		const coverStyle = {
+			width: 128,
+			height: 183,
+			backgroundImage: `url(${book.imageLinks ?
+				book.imageLinks.thumbnail : require('../icons/no-cover.svg')})`,
 
-	return (
-		<div className="book">
-			<div className="book-top">
-				<div
-					className="book-cover"
-					style={coverStyle}>
-				</div>
-				<div className="book-shelf-changer">
-					<SelectShelf
-						shelf={book.shelf}
-						changeShelf={changeShelf}
+		};
+		return (
+			<div className="book">
+				<div className="book-top">
+					<Dimmer.Dimmable blurring
+						as={Image}
+						dimmed={active}
+						dimmer={{ active, content: shelfSelector }}
+						onMouseEnter={this.handleShow}
+						onMouseLeave={this.handleHide}
+						style={coverStyle}
 					/>
 				</div>
+				<div className="book-info-container">
+					{!!book.title &&
+						<div className="book-title">{book.title}</div>
+					}
+					{!!book.authors &&
+						<div className="book-authors">{book.authors[0]}</div>
+					}
+				</div>
 			</div>
-			{!!book.title &&
-				<div className="book-title">{book.title}</div>
-			}
-			{!!book.authors &&
-				<div className="book-authors">{book.authors[0]}</div>
-			}
-		</div>
-	);
-};
+		);
+	}
+}
 
 Book.propTypes = {
 	book: PropTypes.object.isRequired,
